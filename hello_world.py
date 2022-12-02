fafd805a6ebd78e7ecf4a49a3c61024c44c20aa0
Daniil Sotsugov <daniil.sotsugov@gmail.com>
2022-12-02 17:22:58 +0200

    Add first python files

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+name=input("Mis su nimi on? ")
+def hello():
+    print("Hello",name,end="\n")
+    #print ("""ti
+    #loh""") stavit enert mewdu slovami
+    #("tere/kool!") / = enter; // = / 
+    #end="\n" - zakanchivaet stroku enterom
+    ##print(f"Tere{nimi}") - mowet ne rabotat na staroj versii
+    
+    
+if __name__=="__main__":    
+    nimi = "Daniil"
+    hello()
---
+++ num_pluss_num.py
@@ -0,0 +1,9 @@
+def summ():
+    num1 = float(input("Sisesta esimene täisarv: "))
+    num2 = float(input("Sisesta teine täisarv: "))
+    sum_of_2 = num1 + num2
+    print(f"{num1} + {num2} = {sum_of_2}")
+
+
+if __name__ == "__main__":
+    summ()
