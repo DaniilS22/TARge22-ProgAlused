fd086d4df513d69358483bbe67c2f06445ceef69
Daniil Sotsugov <daniil.sotsugov@gmail.com>
2022-12-02 17:29:05 +0200

    Add docstrings to hello_world module

--- hello_world.py
+++ hello_world.py
@@ -11,3 +11,8 @@ def hello():
 if __name__=="__main__":    
     nimi = "Daniil"
     hello()
+    #print ("""ti
+    #loh""") stavit enert mewdu slovami
+    #("tere/kool!") / = enter; // = / 
+    #end="\n" - zakanchivaet stroku enterom
+    ##print(f"Tere{nimi}") - mowet ne rabotat na staroj versii
